fb93be7e6eca8de8e6dd23a3f607c597134c34a9
Atsuhiro <atcheri@gmail.com>
2025-03-10 21:22:09 +0100

    refactor: add mutex to in memory store to prevent race condition
    Refactor phase

--- internal/infrastructure/persistence/player_store.go
+++ internal/infrastructure/persistence/player_store.go
@@ -1,18 +1,26 @@
 package memorystore
 
+import "sync"
+
 type InMemoryPlayerStore struct {
 	score map[string]int
+	mu    sync.Mutex
 }
 
 func NewInMemoryPlayerStore() InMemoryPlayerStore {
-	return InMemoryPlayerStore{make(map[string]int)}
+	return InMemoryPlayerStore{score: make(map[string]int)}
 }
 
 func (s *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	s.mu.Lock()
+	defer s.mu.Unlock()
 	return s.score[name]
 }
 
 func (s *InMemoryPlayerStore) RecordWin(name string) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	s.score[name]++
 	return nil
 }
