e97751a77dc6c91e2d47a6658e819844b5408fd7
Atsuhiro <atcheri@gmail.com>
2025-03-10 21:14:30 +0100

    test: add integration test
    Red phase

---
+++ internal/infrastructure/http/server_integration_test.go
@@ -0,0 +1,33 @@
+package server_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	server "github.com/atcheri/player-server-web-app-tdd-go/internal/infrastructure/http"
+	memorystore "github.com/atcheri/player-server-web-app-tdd-go/internal/infrastructure/persistence"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRecordWinsAndRetrievePlayerScore(t *testing.T) {
+	// arrange
+	store := memorystore.InMemoryPlayerStore{}
+	server := server.PlayerServer{&store}
+
+	// act
+	postRequest, _ := http.NewRequest(http.MethodPost, "/players/Pepper", nil)
+	server.ServeHTTP(httptest.NewRecorder(), postRequest)
+	server.ServeHTTP(httptest.NewRecorder(), postRequest)
+	server.ServeHTTP(httptest.NewRecorder(), postRequest)
+
+	response := httptest.NewRecorder()
+
+	getRequest, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
+	server.ServeHTTP(response, getRequest)
+
+	// assert
+	assert.Equal(t, response.Code, http.StatusOK)
+
+	assert.Equal(t, response.Body.String(), "3")
+}
--- internal/infrastructure/persistence/player_store.go
+++ internal/infrastructure/persistence/player_store.go
@@ -5,3 +5,7 @@ type InMemoryPlayerStore struct{}
 func (s *InMemoryPlayerStore) GetPlayerScore(name string) int {
 	return 123
 }
+
+func (s *InMemoryPlayerStore) RecordWin(name string) error {
+	return nil
+}
