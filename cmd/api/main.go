1c1aecce0f36b2a7831018676f18e54e3f358f37
Atsuhiro <atcheri@gmail.com>
2025-03-10 15:43:24 +0100

    refactor: add an inmemory store for the main app

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,9 +5,10 @@ import (
 	"net/http"
 
 	server "github.com/atcheri/player-server-web-app-tdd-go/internal/infrastructure/http"
+	memorystore "github.com/atcheri/player-server-web-app-tdd-go/internal/infrastructure/persistence"
 )
 
 func main() {
-	srv := &server.PlayerServer{}
+	srv := &server.PlayerServer{Store: &memorystore.InMemoryPlayerStore{}}
 	log.Fatal(http.ListenAndServe(":5000", srv))
 }
--- internal/infrastructure/persistence/player_store.go
+++ internal/infrastructure/persistence/player_store.go
@@ -1,1 +1,7 @@
 package memorystore
+
+type InMemoryPlayerStore struct{}
+
+func (s *InMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
