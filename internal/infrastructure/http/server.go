18b7b5d22d0a678490f45f36ef60ddf7b0668f08
Atsuhiro <atcheri@gmail.com>
2025-03-10 16:55:22 +0100

    refactor: move post and get logic into their own functions
    Refactor phase

--- internal/infrastructure/http/server.go
+++ internal/infrastructure/http/server.go
@@ -13,11 +13,21 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		w.WriteHeader(http.StatusAccepted)
-		return
+	switch r.Method {
+	case http.MethodPost:
+		p.processPlayerWins(w, r)
+	case http.MethodGet:
+		p.showPlayerScore(w, r)
 	}
 
+}
+
+func (p *PlayerServer) processPlayerWins(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusAccepted)
+
+}
+
+func (p *PlayerServer) showPlayerScore(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 
 	score := p.Store.GetPlayerScore(player)
