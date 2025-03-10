c82af7339a9ba7e64f59b575ef62e87a9c284cb1
Atsuhiro <atcheri@gmail.com>
2025-03-10 21:06:35 +0100

    refactor: extract player's name in the handler function
    Refactor phase

--- internal/infrastructure/http/server.go
+++ internal/infrastructure/http/server.go
@@ -13,23 +13,22 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
 	switch r.Method {
 	case http.MethodPost:
-		p.processPlayerWins(w, r)
+		p.processPlayerWins(w, player)
 	case http.MethodGet:
-		p.showPlayerScore(w, r)
+		p.showPlayerScore(w, player)
 	}
 }
 
-func (p *PlayerServer) processPlayerWins(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
+func (p *PlayerServer) processPlayerWins(w http.ResponseWriter, player string) {
 	_ = p.Store.RecordWin(player)
 	w.WriteHeader(http.StatusAccepted)
 
 }
 
-func (p *PlayerServer) showPlayerScore(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
+func (p *PlayerServer) showPlayerScore(w http.ResponseWriter, player string) {
 	score := p.Store.GetPlayerScore(player)
 	if score == 0 {
 		w.WriteHeader(http.StatusNotFound)
