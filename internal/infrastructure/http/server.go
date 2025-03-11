aadc5663b1d95daab69dabdeb33d4e6a811d431b
Atsuhiro <atcheri@gmail.com>
2025-03-11 14:20:54 +0100

    feat: add the mux router
    Green phase

--- internal/infrastructure/http/server.go
+++ internal/infrastructure/http/server.go
@@ -13,13 +13,23 @@ type PlayerServer struct {
 }
 
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	player := strings.TrimPrefix(r.URL.Path, "/players/")
-	switch r.Method {
-	case http.MethodPost:
-		p.processPlayerWins(w, player)
-	case http.MethodGet:
-		p.showPlayerScore(w, player)
-	}
+	router := http.NewServeMux()
+
+	router.Handle("/league", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	router.Handle("/players/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		player := strings.TrimPrefix(r.URL.Path, "/players/")
+		switch r.Method {
+		case http.MethodPost:
+			p.processPlayerWins(w, player)
+		case http.MethodGet:
+			p.showPlayerScore(w, player)
+		}
+	}))
+
+	router.ServeHTTP(w, r)
 }
 
 func (p *PlayerServer) processPlayerWins(w http.ResponseWriter, player string) {
