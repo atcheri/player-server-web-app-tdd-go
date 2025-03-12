75b738e6767c358235166354e60c420155258f57
Atsuhiro <atcheri@gmail.com>
2025-03-12 15:16:25 +0100

    feat: integrate poker cli into main

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	poker "github.com/atcheri/player-server-web-app-tdd-go/internal/infrastructure/cli"
+	"github.com/atcheri/player-server-web-app-tdd-go/internal/infrastructure/persistence"
+)
+
+const dbFileName = "game.db.json"
 
 func main() {
 	fmt.Println("Let's play poker")
+	fmt.Println("Type {Name} wins to record a win")
+
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFileName, err)
+	}
+
+	store, err := persistence.NewFileSystemPlayerStore(db)
+
+	if err != nil {
+		log.Fatalf("problem creating file system player store, %v ", err)
+	}
+
+	game := poker.CLI{PlayerStore: store, In: os.Stdin}
+	game.PlayPoker()
 }
