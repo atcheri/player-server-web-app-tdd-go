a8cd8084c5e31332e47c64df763ddbe0d5461b46
Atsuhiro <atcheri@gmail.com>
2025-03-12 15:04:04 +0100

    test: minimum change to make the test pass
    Green phase

--- internal/infrastructure/cli/cli.go
+++ internal/infrastructure/cli/cli.go
@@ -1,11 +1,16 @@
 package poker
 
-import "github.com/atcheri/player-server-web-app-tdd-go/internal/domain"
+import (
+	"io"
+
+	"github.com/atcheri/player-server-web-app-tdd-go/internal/domain"
+)
 
 type CLI struct {
 	PlayerStore domain.PlayerStore
+	In          io.Reader
 }
 
 func (cli *CLI) PlayPoker() {
-	cli.PlayerStore.RecordWin("Tester")
+	cli.PlayerStore.RecordWin("Chris")
 }
