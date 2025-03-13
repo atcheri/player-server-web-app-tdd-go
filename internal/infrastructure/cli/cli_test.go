b565a96a4567fe6c8954211de95e901d8e920bcc
Atsuhiro <atcheri@gmail.com>
2025-03-13 15:59:52 +0100

    refactor: create a struct for the table driven test case
    Refactor phase

--- internal/infrastructure/cli/cli_test.go
+++ internal/infrastructure/cli/cli_test.go
@@ -32,18 +32,21 @@ func (s *StubPlayerStore) GetLeague() domain.League {
 
 var dummySpyAlerter = &SpyBlindAlerter{}
 
+type scheduledAlert struct {
+	at     time.Duration
+	amount int
+}
+
+func (s scheduledAlert) String() string {
+	return fmt.Sprintf("%d chips at %v", s.amount, s.at)
+}
+
 type SpyBlindAlerter struct {
-	alerts []struct {
-		scheduledAt time.Duration
-		amount      int
-	}
+	alerts []scheduledAlert
 }
 
 func (s *SpyBlindAlerter) ScheduleAlertAt(duration time.Duration, amount int) {
-	s.alerts = append(s.alerts, struct {
-		scheduledAt time.Duration
-		amount      int
-	}{duration, amount})
+	s.alerts = append(s.alerts, scheduledAlert{duration, amount})
 }
 
 func TestCLI(t *testing.T) {
@@ -81,10 +84,7 @@ func TestCLI(t *testing.T) {
 		cli := poker.NewCLI(playerStore, in, blindAlerter)
 		cli.PlayPoker()
 
-		cases := []struct {
-			expectedScheduleTime time.Duration
-			expectedAmount       int
-		}{
+		cases := []scheduledAlert{
 			{0 * time.Second, 100},
 			{10 * time.Minute, 200},
 			{20 * time.Minute, 300},
@@ -99,11 +99,11 @@ func TestCLI(t *testing.T) {
 		}
 
 		for i, c := range cases {
-			t.Run(fmt.Sprintf("%d scheduled for %v", c.expectedAmount, c.expectedScheduleTime), func(t *testing.T) {
+			t.Run(fmt.Sprintf("%d scheduled for %v", c.amount, c.at), func(t *testing.T) {
 				alert := blindAlerter.alerts[i]
 				assert.LessOrEqual(t, i, len(blindAlerter.alerts))
-				assert.Equal(t, c.expectedAmount, alert.amount)
-				assert.Equal(t, alert.scheduledAt, c.expectedScheduleTime)
+				assert.Equal(t, c.amount, alert.amount)
+				assert.Equal(t, alert.at, c.at)
 			})
 		}
 	})
